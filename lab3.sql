298180d2fd15d78e24beee0002081e5f8b54bf7d
Hazel Jane Lihotan <hjlihotan@gmail.com>
2026-08-27 09:49:07 +0800

    Added borrow and return

--- lab3.sql
+++ lab3.sql
@@ -27,23 +27,56 @@ INSERT INTO borrow (student_id, book_id) VALUES
 (2, 1),
 (3, 3);
 
+
+-- Aug 27,2026
 SELECT
     br.borrow_id,
-    s.student_id,
+    
     CONCAT(s.student_first_name, 
     ' ', 
     s.student_last_name
     ) as student_name,
-    s.student_course,
+    
 
     b.book_title,
     b.book_author,
     b.book_category,
 
-    br.borrow_date
+    br.borrow_date,
+    br.borrow_return_date
     FROM borrow br
-    JOIN students s ON br.student_id = s.student_id
+    JOIN students s ON 
+        br.student_id = s.student_id
     JOIN books b ON br.book_id = b.book_id 
 
     WHERE br.borrow_return_date IS NULL
+    ORDER BY br.borrow_date DESC;
+
+    UPDATE borrow
+    SET borrow_return_date = CURRENT_TIMESTAMP
+    WHERE borrow_id = 8 AND borrow_return_date IS NULL;
+
+  -- Set borrrow id for the borrowed book you want to return
+
+SELECT
+    br.borrow_id,
+    
+    CONCAT(s.student_first_name, 
+    ' ', 
+    s.student_last_name
+    ) as student_name,
+    
+
+    b.book_title,
+    b.book_author,
+    b.book_category,
+
+    br.borrow_date,
+    br.borrow_return_date
+    FROM borrow br
+    JOIN students s ON 
+        br.student_id = s.student_id
+    JOIN books b ON br.book_id = b.book_id 
+
+    WHERE br.borrow_return_date IS NOT NULL
     ORDER BY br.borrow_date DESC;
