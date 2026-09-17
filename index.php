5d5cf3dacbe9786994a4f7bfbdaffdb99acc59de
Hazel Jane Lihotan <hjlihotan@gmail.com>
2026-09-17 09:48:06 +0800

    Added initial frontend and initial connection

---
+++ index.php
@@ -0,0 +1,63 @@
+<?php
+// Database Connection
+
+$host = 'localhost';
+$db = 'it30a_lab_db';
+$user = 'root';
+$pass = '';
+$charset = 'utf8mb4';
+
+$dsn = "mysql:host=$host"; dbname=$db; charset="$charset";
+
+$options = [
+    PDO::ATTR_ERRORMODE => PDO:: ERRORMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES=>false 
+];
+try{
+    $pdo = new PDO($dsn,$user,$pass,$options);
+} catch (PDOException $e){
+    die("Database Connection failed" . $e->getMessage();)
+}
+
+//Session
+session_start();
+
+//Deteremine current section
+$section = $_GET['section'] ?? 'students';
+
+//CRUD Operations
+$actions = $GET['action'] ?? '';
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Library Systemt</title>
+</head>
+<body>
+    <h1>Simple Library System<h2>
+        <nav>
+            <a href= "index.php?section=students">Students</a>
+            <a href= "index.php?section=books">Books</a>
+            <a href= "index.php?section=borrow">Borrow</a>
+    </nav>
+    <hr>
+    <?php if($section==='students'): ?>
+        <h1>Students</h1>
+    <?php endif; ?>
+
+     <?php if($section==='books'): ?>
+        <h1>Books</h1>
+    <?php endif; ?>
+
+     <?php if($section==='borrow'): ?>
+        <h1>Borrow</h1>
+    <?php endif; ?>
+    
+</body>
+</html>
+
